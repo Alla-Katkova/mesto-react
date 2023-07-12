88a94b1f23ba922fbff91600db22b138d47dd4c3
Kot <katkova.alla@gmail.com>
2023-07-12 15:50:09 +0300

    fix исправила иерархия в header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,11 @@ import logo from '../../images/logo_mesto.svg'
 export default function Header() {
   return (
     <header className="header">
-    <img
-      src={logo}
-      className="header__logo"
-      alt="лого место"
-    />
-  </header>
+      <img
+        src={logo}
+        className="header__logo"
+        alt="лого место"
+      />
+    </header>
   )
 }
